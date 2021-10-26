4b125ff08c322b10aef49a8e52a241752d923816
RebeckaKla <91217699+RebeckaKla@users.noreply.github.com>
2021-10-26 21:55:22 +0200

    Add files via upload

---
+++ L0002bInl3/L0002bInl3/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace L0002bInl3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(36, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(170, 45);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(306, 45);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 23);
+            this.textBox3.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(306, 102);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ange förnamn:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(170, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Ange efternamn:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(306, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Ange personnummer:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 137);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ L0002bInl3/L0002bInl3/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace L0002bInl3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) //Denna knapp aktiverar metoderna ktr21 samt Female_male samt ber användaren om input
+        {
+            string name = textBox1.Text;
+            string last_name = textBox2.Text;
+            string pnr = textBox3.Text;
+            person.ktr21(pnr);
+        }
+        public class person
+        {
+            string name;
+            string last_name;
+            string pnr;
+
+            public static double ktr21(string pnr) // Denna metod avgör om personnumret är korrekt
+            {
+                int value = 0;
+                for (int i = 0; i < pnr.Length; i++)
+                {
+                    int t = (pnr[i] - 48)
+                        << (1 - (i & 1));
+                    if (t > 9)
+                        t = t - 9;
+                    value += t;
+
+                }
+
+                int svar = value % 10;
+                if (svar == 0) // Om svaret är lika med noll kan vi avgöra att personnumret stämmer
+                {
+                    MessageBox.Show("Personnummer stämmer");
+                    female_male(pnr); // Eftersom personnumret stämmer går programmet vidare och kör nästa metod som avgör vilket kön användaren har
+                }
+                if (svar != 0) // Om svaret inte är lika med noll  så stämmer inte personnumret och programmet kallar inte metoden female_male
+                {
+                    MessageBox.Show("Personnummer stämmer inte");
+                }
+                return (value % 10);
+            }
+
+            static void female_male(string pnr) // Denna metod avgör vilket kön användaren har baserat på den näst sista siffran i personnumret
+            {
+
+                int gender = pnr[8] - 48; // Genom denna variabel markerar man vilken siffra som metoden ska använda sig av för att sedan användas inom if-satsen
+                if ((gender == 1) || (gender == 3) || (gender == 5) || (gender == 7) || (gender == 9)) // De udda siffrorna visar att personen är en man
+                {
+
+                    MessageBox.Show("Man");
+                }
+                if ((gender == 0) || (gender == 2) || (gender == 4) || (gender == 6) || (gender == 8))// De jämna siffrorna visar att personen är en kvinnna 
+                {
+                    MessageBox.Show("Kvinna");
+                }
+            }
+        }
+
+
+    }
+}
